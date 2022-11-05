3f3d098f0687c92c4b634a9f71152080ee886c50
Elio-assaf <assaf.elio1@gmail.com>
2022-11-05 12:32:39 +0100

    .

--- main.py
+++ main.py
@@ -17,31 +17,38 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
 
-        font = 16
+        text_font = QFont("Arial", 16)
         display_font = QFont("Arial", 20)
         # change the font of the textedit
         self.textEdit.setFont(display_font)
+        # max size of the textedit
+        self.textEdit.setMaximumSize(QtCore.QSize(16777215, 450))
+
 
         for i in range(12):
             b1 = QPushButton("Suggestions")
-            b1.setFont(QFont("Arial", font))
+            b1.setFont(text_font)
             # make the buttom expand
             b1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
             y = i % 3
             x = math.floor(i/3)
             self.gridLayout1.addWidget(b1,x,y)
         for i in range(6):
             b1 = QPushButton("Themes")
-            b1.setFont(QFont("Arial", font))
+            b1.setFont(text_font)
             b1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            b1.setMaximumWidth(300)
 
             y = i % 2
             x = math.floor(i/2)
             self.gridLayout2.addWidget(b1,x,y)
         for i in range(4):
             b1 = QPushButton("Custom")
-            b1.setFont(QFont("Arial", font))
+            b1.setFont(text_font)
             b1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            # max width of the button
+            b1.setMaximumWidth(300)
 
             y = i % 2
             x = math.floor(i/2)
@@ -52,7 +59,8 @@ class Window(QMainWindow, Ui_MainWindow):
         self.Setting2.setIcon(icon)
         icon = QIcon(".\icons_gui\close.png")
         self.Setting3.setIcon(icon)
-        icon = QIcon(".\icons_gui\frame.png")
+        icon = QIcon(".\icons_gui\\frame.png")
+        # icon = QIcon(".\icons_gui\frame.png")
         self.Setting4.setIcon(icon)
         icon = QIcon(".\icons_gui\plus.png")
         self.Setting5.setIcon(icon)
@@ -61,6 +69,11 @@ class Window(QMainWindow, Ui_MainWindow):
         icon = QIcon(".\icons_gui\speaker.png")
         self.Setting1.setIcon(icon)
 
+        # loop through Setting1 to 6 and make the botton expand
+        for i in range(6):
+            b1 = getattr(self, "Setting" + str(i+1))
+            b1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            b1.setMaximumWidth(300)
     
 
         # adapt size of icon
