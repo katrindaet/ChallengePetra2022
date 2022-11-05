3eda661099e3a92912a54a2bf3f4fd5e3f21b449
rgonzalezvalino <117526552+rgonzalezvalino@users.noreply.github.com>
2022-11-05 18:41:24 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -23,3 +23,9 @@ class Database:
 
     with open('sentences.json', 'w') as f:
       json.dump(self.data, f)
+
+  def add_new_context(self, new_context):
+    self.data['Contexts'][new_context] = []
+
+  def add_new_sentence(self, context, new_sentence):
+    self.data['Contexts'][context].append(new_sentence)
